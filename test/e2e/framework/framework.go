208b66abcf204315ed299f33f4e267dbbd507520
Yanbing Wang <yawang@microsoft.com>
2020-08-03 12:22:23 -0700

    remove api error

--- test/e2e/framework/framework.go
+++ test/e2e/framework/framework.go
@@ -34,7 +34,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 
 	v1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	//apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
