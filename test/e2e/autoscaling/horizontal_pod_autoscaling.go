792f9709d5dd31c9b95023cbb3c4d80fdcba82e7
Yanbing Wang <yawang@microsoft.com>
2020-09-24 23:03:06 -0700

    revert

--- test/e2e/autoscaling/horizontal_pod_autoscaling.go
+++ test/e2e/autoscaling/horizontal_pod_autoscaling.go
@@ -72,7 +72,7 @@ var _ = SIGDescribe("[Feature:HPA] Horizontal pod autoscaling (scale resource: C
 				initPods:                    1,
 				totalInitialCPUUsage:        150,
 				perPodCPURequest:            200,
-				targetCPUUtilizationPercent: 20,
+				targetCPUUtilizationPercent: 50,
 				minPods:                     1,
 				maxPods:                     2,
 				firstScale:                  2,
